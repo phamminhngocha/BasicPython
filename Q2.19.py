2d7596a58cfe58763b9e9fecd98e3acf747977d7
Phạm Minh Ngọc Hà <73472735+phamminhngocha@users.noreply.github.com>
2024-07-29 03:37:54 +0700

    Create Q2.19.py

---
+++ Q2.19.py
@@ -0,0 +1,10 @@
+#Q2.19
+# Nhập hai số nguyên từ bàn phím
+a = int(input("Nhập số nguyên a: "))
+b = int(input("Nhập số nguyên b: "))
+
+# Kiểm tra xem có ít nhất một số lẻ hay không
+if a % 2 != 0 or b % 2 != 0:
+    print("Trong 2 số a, b có ít nhất một số lẻ")
+else:
+    print("2 số a, b đều là số chẵn")
