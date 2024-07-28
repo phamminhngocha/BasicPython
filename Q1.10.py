ac04cf9eef18081c6e1c1bd7591652a9a1cfcbd8
Phạm Minh Ngọc Hà <73472735+phamminhngocha@users.noreply.github.com>
2024-07-28 21:34:17 +0700

    Create Q1.10.py

---
+++ Q1.10.py
@@ -0,0 +1,15 @@
+#Q1.10
+Ckt='Học viện Tài chính'
+print(Ckt)
+print(Ckt[0]) # Lấy 1 ký tự bên trái của biến Ckt
+print(Ckt[-1]) # Lấy 1 ký tự bên phải của biến Ckt
+print(Ckt[:3]) # Lấy 3 ký tự bên trái của biến Ckt
+print(Ckt[-5:]) # Lấy 5 ký tự bên phải của biến Ckt
+print(Ckt[1:6]) # Lấy từ ký tự thứ 2 đến ký tự thứ 6 có trong biến Ckt (Tính từ trái qua phải)
+print(Ckt[-7:-1]) # Lấy từ ký tự thứ 2 đến ký tự thứ 7 có trong biến Ckt (Tính từ phải qua trái)
+print(Ckt[4:]) # Lấy từ ký tự thứ 5 đến ký tự cuối cùng có trong biến Ckt (Tính từ trái qua phải)
+print(Ckt[:-6]) # Lấy từ ký tự thứ 7 đến ký tự cuối cùng có trong biến Ckt (Tính từ phải qua trái)
+print(Ckt.replace('c v','c123 v') # Chèn chuỗi ký tự '123' vào sau từ 'Học' của biên Ckt
+print(Ckt[::-1]) # Tạo chuỗi ký tự đảo ngược với chuỗi ký tự Ckt
+print(Ckt[::2]) # Lấy các ký tự ở các vị trí lẻ có trong Ckt
+print(Ckt.replace(" ",""))# Xóa các ký tự trống có trong Ckt
