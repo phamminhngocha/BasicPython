2b02c758fcc38a7396a8977c357a7073511ee127
Phạm Minh Ngọc Hà <73472735+phamminhngocha@users.noreply.github.com>
2024-07-29 03:38:04 +0700

    Create Q2.20.py

---
+++ Q2.20.py
@@ -0,0 +1,13 @@
+#Q2.20
+# Nhập số thực a từ người dùng
+a = float(input("Nhập vào một số thực a: "))
+
+# Kiểm tra xem phần thập phân của a có bằng 0 không
+if a == int(a):
+    # Nếu phần thập phân bằng 0 thì a là số nguyên
+    if a > 0:
+        print("a là số nguyên dương")
+    else:
+        print("a là số nguyên âm")
+else:
+    print("a không là số nguyên")
